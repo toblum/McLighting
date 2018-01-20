6f156290724ea1171131cdfd2ae324cef4a7e75a
Tobias Blum <github@tobiasblum.de>
2018-01-21 00:45:03 +0100

    Disable button by default

--- Arduino/McLighting/definitions.h
+++ Arduino/McLighting/definitions.h
@@ -8,7 +8,7 @@ const char HOSTNAME[] = "ESP8266_01";   // Friedly hostname
 
 #define ENABLE_OTA    // If defined, enable Arduino OTA code.
 #define ENABLE_MQTT   // If defined, enable MQTT client code.
-#define ENABLE_BUTTON  // If defined, enable button handling code.
+// #define ENABLE_BUTTON  // If defined, enable button handling code.
 
 // parameters for automatically cycling favorite patterns
 uint32_t autoParams[][4] = {   // color, speed, mode, duration (seconds)
